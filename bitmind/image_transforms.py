aab3b607f2aff299b024a55a9d15a20ba56bf8b1
benliang99 <caliangben@gmail.com>
2024-11-26 04:19:42 +0000

    Fixed comma

--- bitmind/image_transforms.py
+++ bitmind/image_transforms.py
@@ -153,7 +153,7 @@ ucf_transforms = transforms.Compose([
     CenterCrop(),
     transforms.Resize(TARGET_IMAGE_SIZE),
     #CLAHE(),
-    transforms.ToTensor()
+    transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     #TensorCLAHE()
 ])
